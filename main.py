4b1de07c892a823e248a313f9359a9adc32d4d3e
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-12-28 22:46:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("ボタン固定の例")
-st.write("長いコンテンツ..." * 100) # スクロール用
-
-# 画面最下部の領域にボタンを配置
-with st._bottom:
-    if st.button("送信"):
-        st.write("ボタンが押されました")
+with st.sidebar:
+    st.title("ボタン固定の例")
+    st.write("長いコンテンツ..." * 100) # スクロール用
+    
+    # 画面最下部の領域にボタンを配置
+    with st._bottom:
+        if st.button("送信"):
+            st.write("ボタンが押されました")
